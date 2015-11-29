2e11eed77db0e74e817778ca5c08a7ec19f6b628
Kacper Walanus <kacper@walanus.com>
2015-11-29 14:33:01 +0100

    display links to commits

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,15 +1,45 @@
 class Commit
   class << self
 
-    def by_date(date = Date.today)
-      Octokit.commits_on(repo, date)
+    def by_date(date = 2.days.ago)
+      Octokit.commits_on(repo, date).map(&method(:to_item)).reject(&:merge_commit?).sort
     end
 
-    private
+    def to_item(resource)
+      Item.new(resource)
+    end
 
     def repo
       ENV.fetch('GITHUB_REPO')
     end
 
+  end
+
+  class Item < Sawyer::Resource
+
+    include Decorator
+
+    def issue_html_url
+      "https://github.com/#{Commit.repo}/issues/#{issue_number}" if issue_number
+    end
+
+    def <=>(other)
+      return 1 unless issue_number
+      return -1 unless other.issue_number
+      issue_number <=> other.issue_number
+    end
+
+    def issue_number
+      match = commit.message.match(/#(\d+)/)
+      match[1] if match
+    end
+
+    def merge_commit?
+      commit.message.match(/Merge/)
+    end
+
+    def commit_message
+      commit.message
+    end
   end
 end
