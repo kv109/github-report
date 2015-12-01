d38425b461048a8d6431a9fd35e7f72c22711302
Kacper Walanus <kacper@walanus.com>
2015-12-01 15:35:40 +0100

    refactor

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,33 +1,35 @@
 class Event
-  def self.by_organization_and_user
-    @collection = Octokit.paginate("#{base_uri}orgs/#{org}", per_page: 1000)
-    self
-  end
+  class << self
+    def by_organization_and_user
+      @collection = Octokit.paginate("#{base_uri}orgs/#{org}", per_page: 1000)
+      self
+    end
 
-  def self.issues
-    @collection.map(&method(:to_item)).select(&:issues_event?)
-  end
+    def issues
+      @collection.map(&method(:to_item)).select(&:issues_event?)
+    end
 
-  def self.to_item(resource)
-    Item.new(resource)
-  end
+    def to_item(resource)
+      Item.new(resource)
+    end
 
-  def self.get
-    @collection
-  end
+    def get
+      @collection
+    end
 
-  private
+    private
 
-  def self.base_uri
-    "#{Octokit::User.path login}/events/"
-  end
+    def base_uri
+      "#{Octokit::User.path login}/events/"
+    end
 
-  def self.login
-    @login ||= Octokit.user.login
-  end
+    def login
+      @login ||= Octokit.user.login
+    end
 
-  def self.org
-    ENV['GITHUB_ORG']
+    def org
+      ENV['GITHUB_ORG']
+    end
   end
 
   class Item < Sawyer::Resource
