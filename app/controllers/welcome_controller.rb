582a2297c9e641b6d51769acd1b412ee3bad1186
Kacper Walanus <kacper@walanus.com>
2015-12-09 19:02:44 +0100

    remove old code

--- app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-
-  def index
-    @commits = Commit.new(@client).by_repo_and_date(current_repo_full_name!).get
-    @events = []
-    # @events = Event.by_organization_and_user.get
-  end
-
-end
