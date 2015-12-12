5b308efe4cce213322fa4f3e1b7e1cee873d0453
Kacper Walanus <kacper@walanus.com>
2015-12-12 12:45:26 +0100

    ajaxify collaborators page

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -8,8 +8,12 @@ class ReposController < ApplicationController
   end
 
   def show
+  end
+
+  def show_partial
     @repo_full_name = current_repo_owner! + '/' + current_repo!
     @collaborators = Collaborator.new(@client).by_repo(@repo_full_name).get
+    render partial: 'repos/show'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
 
-  get '/repos/_index' => 'repos#index_partial', as: :repos_partial
+  get '/repos/index/_' => 'repos#index_partial', as: :repos_partial
   get '/:owner/:repo/' => 'repos#show', as: :show_repo
+  get '/:owner/:repo/_' => 'repos#show_partial', as: :show_repo_partial
   get '/:owner/:repo/:user' => 'users#show', as: :user_report
 end
