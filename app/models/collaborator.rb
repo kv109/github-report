37af321bbd4b88c7d3a23e00be90243d647dfce9
Kacper Walanus <kacper@walanus.com>
2015-12-13 14:19:59 +0100

    sort contributors based on activity

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -3,13 +3,21 @@ class Collaborator
   include Query
 
   def by_repo(repo)
-    @query = [:collaborators, repo]
+    @query = [:contributors_stats, repo]
     self
+  end
+
+  def send_query(query)
+    super.reverse
   end
 
   private
 
   class Item
     include Decorator
+
+    def login
+      author.login
+    end
   end
 end
